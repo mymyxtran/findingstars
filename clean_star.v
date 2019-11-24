a5678dc2bf0c66263579c1a72a9b04b0b06adfe5
kelwerks <47065173+kelwerks@users.noreply.github.com>
2019-11-24 10:41:31 -0500

    Update clean_star.v

--- clean_star.v
+++ clean_star.v
@@ -30,18 +30,18 @@ module clean_star(goClean, xLeft, xRight, yTop, yBottom, clk, xOut, yOut, colOut
 		
 	cleanControl cleanfsm0(.clk(clk), .resetn(resetn), .xEdge(xEdge), .yEdge(yEdge), .goClean(goClean), 
 						.countXEn(countXEn), .countYEn(countYEn), .ld_x(ld_x), .ld_y(ld_y), .doneClean(doneClean), .wrEn(wrEn));
-		
+	
 
 endmodule
 
 module cleanDataPath(xLeft, xRight, yTop, yBottom, countXEn, countYEn, ld_x, ld_y, clk, resetn,
-							xEdge, yEdge, xOut, yOut, colOut);
+							xEdge, yEdge, addressOut, colOut);
 
 	//contants; depend on image size and colour res
 	parameter xSz = 8;
 	parameter ySz = 7;
 	parameter colSz = 3;
-	
+	parameter addrSz = 15;
 	//these define the dimensions of the box
 	input[xSz-1:0] xLeft, xRight;
 	input[ySz-1:0] yTop, yBottom;
@@ -57,8 +57,7 @@ module cleanDataPath(xLeft, xRight, yTop, yBottom, countXEn, countYEn, ld_x, ld_
 	
 	//output to VGA adapter: note that this module already contains an address translator... but may need to remake depending 
 	//on img size!
-	output[xSz-1:0] xOut;
-	output[ySz-1:0] yOut;
+	output[addrSz-1:0] addressOut;
 	output[colSz-1:0] colOut;
 	
 	//assign to black
@@ -90,13 +89,12 @@ module cleanDataPath(xLeft, xRight, yTop, yBottom, countXEn, countYEn, ld_x, ld_
 				yCount <= yCount + 1;			
 	end
 	
-	assign xOut = xCount;
-	
 	//has the y edge been reached?
 	assign yEdge = (yCount == yBottom);
-	
-	assign yOut = yCount;
 
+	vga_address_translator trans1(.x(xCount),.y(yCount),.mem_address(addressOut));
+	
+	
 endmodule
 
 
@@ -202,3 +200,38 @@ module cleanControl(input clk, xEdge, yEdge, goClean, output reg countXEn, count
 	
 endmodule
 
+/* This module converts a user specified coordinates into a memory address.
+ * The output of the module depends on the resolution set by the user.
+ */
+module vga_address_translator(x, y, mem_address);
+
+	parameter RESOLUTION = "160x120";
+	/* Set this parameter to "160x120" or "320x240". It will cause the VGA adapter to draw each dot on
+	 * the screen by using a block of 4x4 pixels ("160x120" resolution) or 2x2 pixels ("320x240" resolution).
+	 * It effectively reduces the screen resolution to an integer fraction of 640x480. It was necessary
+	 * to reduce the resolution for the Video Memory to fit within the on-chip memory limits.
+	 */
+
+	input [((RESOLUTION == "320x240") ? (8) : (7)):0] x; 
+	input [((RESOLUTION == "320x240") ? (7) : (6)):0] y;	
+	output reg [((RESOLUTION == "320x240") ? (16) : (14)):0] mem_address;
+	
+	/* The basic formula is address = y*WIDTH + x;
+	 * For 320x240 resolution we can write 320 as (256 + 64). Memory address becomes
+	 * (y*256) + (y*64) + x;
+	 * This simplifies multiplication a simple shift and add operation.
+	 * A leading 0 bit is added to each operand to ensure that they are treated as unsigned
+	 * inputs. By default the use a '+' operator will generate a signed adder.
+	 * Similarly, for 160x120 resolution we write 160 as 128+32.
+	 */
+	wire [16:0] res_320x240 = ({1'b0, y, 8'd0} + {1'b0, y, 6'd0} + {1'b0, x});
+	wire [15:0] res_160x120 = ({1'b0, y, 7'd0} + {1'b0, y, 5'd0} + {1'b0, x});
+	
+	always @(*)
+	begin
+		if (RESOLUTION == "320x240")
+			mem_address = res_320x240;
+		else
+			mem_address = res_160x120[14:0];
+	end
+endmodule
