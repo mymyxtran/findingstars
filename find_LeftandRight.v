9d59076fee6e3ea5916d1ddf4e5947f3b6c1ddc6
mymytraan <57696924+mymytraan@users.noreply.github.com>
2019-11-24 10:36:27 -0500

    Update find_LeftandRight.v

--- find_LeftandRight.v
+++ find_LeftandRight.v
@@ -517,12 +517,21 @@ end
 	
 endmodule
 
-module address_translator(x, y, mem_address);
-	
-	// 60 x 60
-	input [5:0] x; 
-	input [5:0] y;	
-	output [11:0] mem_address;
+/* This module converts a user specified coordinates into a memory address.
+ * The output of the module depends on the resolution set by the user.
+ */
+module vga_address_translator(x, y, mem_address);
+
+	parameter RESOLUTION = "160x120";
+	/* Set this parameter to "160x120" or "320x240". It will cause the VGA adapter to draw each dot on
+	 * the screen by using a block of 4x4 pixels ("160x120" resolution) or 2x2 pixels ("320x240" resolution).
+	 * It effectively reduces the screen resolution to an integer fraction of 640x480. It was necessary
+	 * to reduce the resolution for the Video Memory to fit within the on-chip memory limits.
+	 */
+
+	input [((RESOLUTION == "320x240") ? (8) : (7)):0] x; 
+	input [((RESOLUTION == "320x240") ? (7) : (6)):0] y;	
+	output reg [((RESOLUTION == "320x240") ? (16) : (14)):0] mem_address;
 	
 	/* The basic formula is address = y*WIDTH + x;
 	 * For 320x240 resolution we can write 320 as (256 + 64). Memory address becomes
@@ -532,8 +541,15 @@ module address_translator(x, y, mem_address);
 	 * inputs. By default the use a '+' operator will generate a signed adder.
 	 * Similarly, for 160x120 resolution we write 160 as 128+32.
 	 */
-	 //width = 60 = 32 + 16 + 8 + 4
-	 //so address = (y*32) + (y*16)  + (y*8)+ (y*4)+ x
-	 assign mem_address = ({1'b0, y, 5'd0} + {1'b0, y, 4'd0} +{1'b0, y, 3'd0} + {1'b0, y, 2'd0} + {1'b0,x});
+	wire [16:0] res_320x240 = ({1'b0, y, 8'd0} + {1'b0, y, 6'd0} + {1'b0, x});
+	wire [15:0] res_160x120 = ({1'b0, y, 7'd0} + {1'b0, y, 5'd0} + {1'b0, x});
 	
+	always @(*)
+	begin
+		if (RESOLUTION == "320x240")
+			mem_address = res_320x240;
+		else
+			mem_address = res_160x120[14:0];
+	end
 endmodule
+
