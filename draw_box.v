fc9c6280593b2af0cc42b0cf1af77d98e51739f7
mymytraan <57696924+mymytraan@users.noreply.github.com>
2019-11-24 10:19:24 -0500

    Update draw_box.v

--- draw_box.v
+++ draw_box.v
@@ -3,8 +3,8 @@
 module draw_box(goDraw, xLeft, xRight, yTop, yBottom, clk, xOut, yOut, colOut, doneDraw,plotEn);
 
 	//contants; depend on image size and colour res
-	parameter xSz = 3;
-	parameter ySz = 3;
+	parameter xSz = 8;
+	parameter ySz = 7;
 	parameter colSz = 3;
 	
 	//these define the dimensions of the box
